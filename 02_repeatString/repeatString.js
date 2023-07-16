ed926b80da7e76d50e03dd3426a261f84b4e843f
aian <53809101+deeowemez@users.noreply.github.com>
2023-07-16 20:35:08 +0800

    do 01_helloWorld and 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = '';
+    if (num < 0){
+        return "ERROR";
+    }
+    if (string === ''){
+        return '';
+    }
+    for (let i = 0; i < num; i++){
+        result += string;
+    }
+    return result
 };
+
+repeatString("", 10)
 
 // Do not edit below this line
 module.exports = repeatString;
